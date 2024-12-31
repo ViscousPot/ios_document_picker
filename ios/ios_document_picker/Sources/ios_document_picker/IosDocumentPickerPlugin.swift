1044b457d13ce31fb726a15fa253eec232e8a3c8
Mgen <m@mgenware.com>
2024-12-31 23:57:10 +0800

    Add Swift Package Manager support

--- ios/ios_document_picker/Sources/ios_document_picker/IosDocumentPickerPlugin.swift
+++ ios/ios_document_picker/Sources/ios_document_picker/IosDocumentPickerPlugin.swift
@@ -6,41 +6,43 @@ enum PickerMode: Int { case file, folder }
 
 public class IosDocumentPickerPlugin: NSObject, FlutterPlugin, UIDocumentPickerDelegate {
   var resultFn: FlutterResult?
-  
+
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ios_document_picker", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(
+      name: "ios_document_picker", binaryMessenger: registrar.messenger())
     let instance = IosDocumentPickerPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    guard let args = call.arguments as? Dictionary<String, Any> else {
+    guard let args = call.arguments as? [String: Any] else {
       result(FlutterError(code: "InvalidArgsType", message: "Invalid args type", details: nil))
       return
     }
     switch call.method {
     case "pick":
       resultFn = result
-      
+
       let mode = PickerMode(rawValue: args["type"] as! Int)
       let allowsMultiple = args["multiple"] as? Bool ?? false
       let allowedUtiTypes = args["allowedUtiTypes"] as? [String]
-      
+
       let utTypes = allowedUtiTypes?.compactMap { UTType($0) }
-      
+
       let documentPicker =
-      UIDocumentPickerViewController(forOpeningContentTypes: utTypes ?? (mode == .folder ? [UTType.folder] : [UTType.data]))
+        UIDocumentPickerViewController(
+          forOpeningContentTypes: utTypes ?? (mode == .folder ? [UTType.folder] : [UTType.data]))
       documentPicker.delegate = self
       documentPicker.allowsMultipleSelection = allowsMultiple
 
       // Present the document picker.
       currentViewController()?.present(documentPicker, animated: true, completion: nil)
-      
+
     default:
       result(FlutterMethodNotImplemented)
     }
   }
-  
+
   private func currentViewController() -> UIViewController? {
     var keyWindow: UIWindow?
     for window in UIApplication.shared.windows {
@@ -49,22 +51,24 @@ public class IosDocumentPickerPlugin: NSObject, FlutterPlugin, UIDocumentPickerD
         break
       }
     }
-    
+
     var topController = keyWindow?.rootViewController
     while topController?.presentedViewController != nil {
       topController = topController?.presentedViewController
     }
     return topController
   }
-  
+
   private func urlToMap(_ url: URL) -> [String: String] {
     return ["url": url.absoluteString, "path": url.path, "name": url.lastPathComponent]
   }
-  
-  public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-    resultFn?(urls.map{ urlToMap($0) })
+
+  public func documentPicker(
+    _ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]
+  ) {
+    resultFn?(urls.map { urlToMap($0) })
   }
-  
+
   public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
     resultFn?(nil)
   }
